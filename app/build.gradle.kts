8a9eb5f2139ccf48d4c9eee9f9583ffbc1fce705
sofia <sofiapav159@gmail.com>
2026-05-29 22:45:02 +0300

    jvm 11 -> 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,16 +30,16 @@ android {
         }
     }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
+//    compileOptions {
+//        sourceCompatibility = JavaVersion.VERSION_11
+//        targetCompatibility = JavaVersion.VERSION_11
+//    }
 
     buildToolsVersion = "36.0.0"
 }
 
 kotlin {
-    jvmToolchain(11)
+    jvmToolchain(21)
 }
 
 dependencies {
